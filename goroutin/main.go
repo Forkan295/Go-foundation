b9527508b10b8b8a043c01e7718499ce2be5c92c
forkan <forkanalam295@gmail.com>
2023-05-26 21:04:30 +0600

    go concurrancy with go routine, channel and select

--- goroutin/main.go
+++ goroutin/main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	go fmt.Println("go routing working 1")
-	go fmt.Println("go routing working 2")
-	go fmt.Println("go routing working 3")
-	time.Sleep(time.Second)
-	go fmt.Println("go routing working 4")
-	go fmt.Println("go routing working 5")
-	time.Sleep(time.Second * 3)
-	go fmt.Println("go routing working 6")
-	time.Sleep(time.Second * 3)
-	fmt.Println("go routing working 7")
+	myChannel := make(chan string)
+	//time.Sleep(time.Second)
+	anotherChannel := make(chan string)
+
+	go func() {
+		myChannel <- "mychanal"
+	}()
+
+	go func() {
+		anotherChannel <- "anotherchanal"
+	}()
+
+	select {
+	case printmyChannel := <-myChannel:
+		fmt.Println(printmyChannel)
+	case printanotherChannel := <-anotherChannel:
+		fmt.Println(printanotherChannel)
+	}
 }
